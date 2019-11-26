b97f8e2cf4d7b2f89237ec29f63ce3e2de81b42c
KENNETH NIPPER <knipper@quickrx.net>
2019-11-26 11:33:28 -0500

    Removed obsolete code and updated to .net core

---
+++ ScoutDemoService.Core/Controllers/ScoutDemoController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScoutDemoService.Core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScoutDemoController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Models.PersonModel> Get()
+        {
+            List<Models.PersonModel> PersonList = new List<Models.PersonModel>();
+
+            Models.PersonModel FirstPerson = new Models.PersonModel
+            {
+                FirstName = "Bob",
+                LastName = "Smith",
+                EmailAddress = "anyone@anywhere.com",
+                Address1 = "123 anystreet",
+                Address2 = "Suite 123",
+                City = "Anytown",
+                State = "GA",
+                ZipCode = "12345"
+            };
+
+            PersonList.Add(FirstPerson);
+
+            return PersonList;
+        }
+    }
+}
