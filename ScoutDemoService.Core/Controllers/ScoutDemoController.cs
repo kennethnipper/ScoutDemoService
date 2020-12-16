4104422424a8cf4b3a82bc9b570d40cc658bcbc9
Kenneth Nipper <knipper@quickrx.net>
2020-12-16 15:00:34 -0500

    Converted to .net5.0.  Added more functionality now that I had some free time.  Docker support doesn't currently work due to Docker Desktop 3.0.0 failing to start.  Hopefully that'll be straightened out soon.

--- ScoutDemoService.Core/Controllers/ScoutDemoController.cs
+++ ScoutDemoService.Core/Controllers/ScoutDemoController.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using ScoutDemoService.Core.Models;
 
 namespace ScoutDemoService.Core.Controllers
 {
@@ -7,26 +11,146 @@ namespace ScoutDemoService.Core.Controllers
     [Route("[controller]")]
     public class ScoutDemoController : ControllerBase
     {
-        [HttpGet]
-        public IEnumerable<Models.PersonModel> Get()
+        private string ConnectionString = "Server=scoutdemoservice.db;Uid=root;Pwd=scoutdemopassword1234!;";
+        /*
+         * HttpPost in this format with Content-Type = application/json
+         * https://localhost:44313/ScoutDemo
+         * {"firstName":"Bob","lastName":"Smith","emailAddress":"anyone@anywhere.com","address1":"123 Main St.","address2":"Apt 100","city":"Rome","state":"GA","zipCode":"12345"}
+         */
+        [HttpPost]
+        public IActionResult Index([FromBody]PersonModel Person)
         {
-            List<Models.PersonModel> PersonList = new List<Models.PersonModel>();
-
-            Models.PersonModel FirstPerson = new Models.PersonModel
+            if (InsertPerson(Person) > 0)
             {
-                FirstName = "Bob",
-                LastName = "Smith",
-                EmailAddress = "anyone@anywhere.com",
-                Address1 = "123 anystreet",
-                Address2 = "Suite 123",
-                City = "Anytown",
-                State = "GA",
-                ZipCode = "12345"
-            };
-
-            PersonList.Add(FirstPerson);
-
+                return StatusCode(StatusCodes.Status200OK);
+            }
+            return StatusCode(StatusCodes.Status304NotModified);
+        }
+        [HttpGet]
+        public IEnumerable<PersonModel> Get(Int32? ID)
+        {
+            PopulateEmptyDB();
+            List<PersonModel> PersonList = new List<PersonModel>();
+            if (ID.HasValue)
+            {
+                PersonList.Add(GetPersonByID((Int32)ID));
+            }
+            else
+            {
+                PersonList = GetPeople();
+            }
             return PersonList;
+        }
+        public void PopulateEmptyDB()
+        {
+            using (MySqlConnection Connection = new MySqlConnection(ConnectionString))
+            {
+                using (MySqlCommand Command = new MySqlCommand("CREATE SCHEMA IF NOT EXISTS scoutdemo;", Connection))
+                {
+                    if (Connection.State != System.Data.ConnectionState.Open) { Connection.Open(); }
+                    Command.ExecuteNonQuery();
+                }
+                using (MySqlCommand Command = new MySqlCommand("CREATE TABLE IF NOT EXISTS scoutdemo.people(`id` int NOT NULL AUTO_INCREMENT, " +
+                        "`firstname` varchar(45) DEFAULT NULL, " +
+                        "`lastname` varchar(45) DEFAULT NULL, " +
+                        "`emailaddress` varchar(45) DEFAULT NULL, " +
+                        "`address1` varchar(45) DEFAULT NULL, " +
+                        "`address2` varchar(45) DEFAULT NULL, " +
+                        "`city` varchar(45) DEFAULT NULL, " +
+                        "`state` varchar(45) DEFAULT NULL, " +
+                        "`zipcode` varchar(45) DEFAULT NULL, " +
+                        "PRIMARY KEY (`id`), " +
+                        "UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)", Connection))
+                {
+                    if (Connection.State != System.Data.ConnectionState.Open) { Connection.Open(); }
+                    Command.ExecuteNonQuery();
+                }
+                using (MySqlCommand Command = new MySqlCommand("select count(id) from scoutdemo.people", Connection))
+                {
+                    if (Connection.State != System.Data.ConnectionState.Open) { Connection.Open(); }
+                    if (Int32.Parse(Command.ExecuteScalar().ToString()) == 0)
+                    {
+                        InsertPerson(new PersonModel() { FirstName = "Bob", LastName = "Smith", EmailAddress = "anyone@anywhere.com", Address1 = "123 Main St.", Address2 = "Apt 100", City = "Rome", State = "GA", ZipCode = "12345" });
+                    }
+                }
+            }
+        }
+        public Int32 InsertPerson(PersonModel PM)
+        {
+            using (MySqlConnection Connection = new MySqlConnection(ConnectionString))
+            {
+                using (MySqlCommand Command = new MySqlCommand("insert into scoutdemo.people(firstname, lastname, emailaddress, address1, address2, city, state, zipcode) values (@firstname, @lastname, @emailaddress, @address1, @address2, @city, @state, @zipcode)", Connection))
+                {
+                    if (Connection.State != System.Data.ConnectionState.Open) { Connection.Open(); }
+                    Command.Parameters.Add("@firstname", MySqlDbType.VarChar).Value = PM.FirstName;
+                    Command.Parameters.Add("@lastname", MySqlDbType.VarChar).Value = PM.LastName;
+                    Command.Parameters.Add("@emailaddress", MySqlDbType.VarChar).Value = PM.EmailAddress;
+                    Command.Parameters.Add("@address1", MySqlDbType.VarChar).Value = PM.Address1;
+                    Command.Parameters.Add("@address2", MySqlDbType.VarChar).Value = PM.Address2;
+                    Command.Parameters.Add("@city", MySqlDbType.VarChar).Value = PM.City;
+                    Command.Parameters.Add("@state", MySqlDbType.VarChar).Value = PM.State;
+                    Command.Parameters.Add("@zipcode", MySqlDbType.VarChar).Value = PM.ZipCode;
+                    return Command.ExecuteNonQuery();
+                }
+            }
+        }
+        public List<PersonModel> GetPeople()
+        {
+            List<PersonModel> People = new List<PersonModel>();
+            using (MySqlConnection Connection = new MySqlConnection(ConnectionString))
+            {
+                using (MySqlCommand Command = new MySqlCommand("Select id, firstname, lastname, emailaddress, address1, address2, city, state, zipcode from scoutdemo.people", Connection))
+                {
+                    if (Connection.State != System.Data.ConnectionState.Open) { Connection.Open(); }
+                    using (MySqlDataReader reader = Command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            PersonModel PM = new PersonModel()
+                            {
+                                ID = reader.IsDBNull(reader.GetOrdinal("id")) ? 0 : reader.GetInt32(reader.GetOrdinal("id")),
+                                FirstName = reader.IsDBNull(reader.GetOrdinal("firstname")) ? string.Empty : reader.GetString(reader.GetOrdinal("firstname")),
+                                LastName = reader.IsDBNull(reader.GetOrdinal("lastname")) ? string.Empty : reader.GetString(reader.GetOrdinal("lastname")),
+                                EmailAddress = reader.IsDBNull(reader.GetOrdinal("emailaddress")) ? string.Empty : reader.GetString(reader.GetOrdinal("emailaddress")),
+                                Address1 = reader.IsDBNull(reader.GetOrdinal("address1")) ? string.Empty : reader.GetString(reader.GetOrdinal("address1")),
+                                Address2 = reader.IsDBNull(reader.GetOrdinal("address2")) ? string.Empty : reader.GetString(reader.GetOrdinal("address2")),
+                                City = reader.IsDBNull(reader.GetOrdinal("city")) ? string.Empty : reader.GetString(reader.GetOrdinal("city")),
+                                State = reader.IsDBNull(reader.GetOrdinal("state")) ? string.Empty : reader.GetString(reader.GetOrdinal("state")),
+                                ZipCode = reader.IsDBNull(reader.GetOrdinal("zipcode")) ? string.Empty : reader.GetString(reader.GetOrdinal("zipcode")),
+                            };
+                            People.Add(PM);
+                        }
+                    }
+                }
+            }
+            return People;
+        }
+        public PersonModel GetPersonByID(Int32 ID)
+        {
+            using (MySqlConnection Connection = new MySqlConnection(ConnectionString))
+            {
+                using MySqlCommand Command = new MySqlCommand("Select firstname, lastname, emailaddress, address1, address2, city, state, zipcode from scoutdemo.people where id = @id", Connection);
+                if (Connection.State != System.Data.ConnectionState.Open) { Connection.Open(); }
+                Command.Parameters.Add("@id", MySqlDbType.Int32).Value = ID;
+                using MySqlDataReader reader = Command.ExecuteReader();
+                while (reader.Read())
+                {
+                    PersonModel PM = new PersonModel()
+                    {
+                        ID = ID,
+                        FirstName = reader.IsDBNull(reader.GetOrdinal("firstname")) ? string.Empty : reader.GetString(reader.GetOrdinal("firstname")),
+                        LastName = reader.IsDBNull(reader.GetOrdinal("lastname")) ? string.Empty : reader.GetString(reader.GetOrdinal("lastname")),
+                        EmailAddress = reader.IsDBNull(reader.GetOrdinal("emailaddress")) ? string.Empty : reader.GetString(reader.GetOrdinal("emailaddress")),
+                        Address1 = reader.IsDBNull(reader.GetOrdinal("address1")) ? string.Empty : reader.GetString(reader.GetOrdinal("address1")),
+                        Address2 = reader.IsDBNull(reader.GetOrdinal("address2")) ? string.Empty : reader.GetString(reader.GetOrdinal("address2")),
+                        City = reader.IsDBNull(reader.GetOrdinal("city")) ? string.Empty : reader.GetString(reader.GetOrdinal("city")),
+                        State = reader.IsDBNull(reader.GetOrdinal("state")) ? string.Empty : reader.GetString(reader.GetOrdinal("state")),
+                        ZipCode = reader.IsDBNull(reader.GetOrdinal("zipcode")) ? string.Empty : reader.GetString(reader.GetOrdinal("zipcode")),
+                    };
+                    return PM;
+                }
+            }
+            return null;
         }
     }
 }
